58aa20f23cbed1353eebd9a68399b449d13da156
Olaoluwa22 <olaoluwaoni22@gmail.com>
2023-11-24 00:11:59 +0100

    DeleteMapping added

--- src/main/java/com/ecommerce/backendAtHiit/controller/ProductController.java
+++ src/main/java/com/ecommerce/backendAtHiit/controller/ProductController.java
@@ -7,10 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
@@ -32,11 +29,17 @@ public class ProductController {
         product.setUpdatedAt(productDto.getUpdatedAt());
         product.setActive(productDto.isActive());
         productRepository.save(product);
-        return ResponseEntity.badRequest().body("Input Error");
+        return ResponseEntity.ok().body("Product Created");
     }
     @GetMapping("/products")
     public ResponseEntity<?> getProducts(){
         List<Product> allProduct = productRepository.findAll();
-        return new ResponseEntity<>(allProduct, HttpStatus.CREATED);
+        return new ResponseEntity<>(allProduct, HttpStatus.FOUND);
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable(name ="id") Long productId){
+        productRepository.deleteById(productId);
+        String response="Product Deleted!";
+        return new ResponseEntity<>(response,HttpStatus.OK);
     }
 }
