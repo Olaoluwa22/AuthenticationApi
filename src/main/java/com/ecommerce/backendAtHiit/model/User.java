506f709cfda030892c1e82bbedde89ce3f6a2438
Olaoluwa22 <olaoluwaoni22@gmail.com>
2023-12-05 10:14:47 +0100

    New logic added to the UserController

--- src/main/java/com/ecommerce/backendAtHiit/model/User.java
+++ src/main/java/com/ecommerce/backendAtHiit/model/User.java
@@ -2,7 +2,6 @@ package com.ecommerce.backendAtHiit.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 import java.time.Instant;
